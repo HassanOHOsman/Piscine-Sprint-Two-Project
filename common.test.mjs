4af48fcf49748f50cbf3c6ff5afdd6ca3eb14f01
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-14 22:14:04 +0100

    test functionality of getEventDate function

--- common.test.mjs
+++ common.test.mjs
@@ -1,5 +1,6 @@
 
 import { getDaysInMonth } from "./common.mjs";
+import { getEventDate } from "./populate-calendar.mjs"
 import assert from "node:assert";
 import test from "node:test";
 
@@ -24,4 +25,22 @@ test("Return 29 days for Feb in a leap year", () => {
   assert.equal(getDaysInMonth(2016, 1), 29);
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+
+//(2) Test the functionality of getEventDate
+
+//Recent event
+test("Second Saturday of May 2025 falls on the 10th", () => {
+  assert.equal(getEventDate(2025, "May", "Saturday", "second"), 10);
+});
+
+//Past event
+test("Return the 17th for MLK Day (third Monday of January) in 2000", () => {
+  assert.equal(getEventDate(2000, "January", "Monday", "third"), 17);
+});
+
+//Future event
+test("Return the 19th for the Father's Day (third Sunday of June) in 2050", () => {
+  assert.equal(getEventDate(2050, "June", "Sunday", "third"), 19);
+});

60bad7f45cba92286a3ac909935b0be10f98d66c
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-15 20:57:18 +0100

    importe writeFile to allow saving and creation of iCal file

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,9 +1,8 @@
 // This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
 // It can be run with `node`.
 
-import { getEventDate } from "./populate-calendar.mjs";
-
-
+import { processEventsForCalendar, getEventDate } from "./populate-calendar.mjs";
 import daysData from "./days.json" with { type: "json" };
+import { writeFile } from "node:fs/promises";
 
 
